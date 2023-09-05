fa13e14c9f31e9090f5c66d17d25dcda93612e2a
RachellSalcedo <scholar@paradigm.local>
2023-09-05 13:28:00 -0600

    variables and a function

---
+++ pdm.py
@@ -0,0 +1,30 @@
+person = "Barrett"
+
+
+# for i in range(10):
+    # print(i)
+
+list = ['one', 'two', 'three']
+dictionary = {
+    'name': 'Barrett',
+    'age': 16
+}
+
+
+def is_even_number(number):
+    if number % 2 == 0:
+        print('This is an even number!')
+    else:
+        print('This is an odd number!')
+
+is_even_number(42)
+
+MAIN_CHARACTER = "mario"
+
+main_character = "luigi"
+
+myNameIsNot = "Rachell" #camel case
+
+my_name = "Daniela" #snake case
+
+My_Name = "patricia" #Pascal case
---
+++ variables.py
@@ -0,0 +1,11 @@
+my_name = "Rachell"
+test_score = 100
+was_lunch_tasty = True 
+b_day_classes = ["kedge", "seminar", "computer programming", "math"]
+
+print(f'Hi, my name is {my_name}. I just score {test_score} on my last test. My classes today are {b_day_classes}.')
+
+def add_two_numbers():
+    print(1 + 2) 
+
+add_two_numbers()
