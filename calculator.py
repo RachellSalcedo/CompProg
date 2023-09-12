f6b1e2ba457fc8377af1420ceaed982d239bb9e5
RachellSalcedo <scholar@paradigm.local>
2023-09-12 13:27:38 -0600

    simple calculator

---
+++ calculator.py
@@ -0,0 +1,8 @@
+user_input = input("give me a number")
+user_input = input("give another number")
+
+user_input = input("which operation would you like to use?" "add", "subtract", "multiply", "divide")
+
+def user_input(answer):
+    if answer == "add":
+            def add_two_numbers():
