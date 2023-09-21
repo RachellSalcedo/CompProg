932cdfa2243cc8c18b94f000355b23f49df5254a
RachellSalcedo <scholar@paradigm.local>
2023-09-21 13:16:04 -0600

    intro to control

--- Control.py
+++ Control.py
@@ -1,26 +1,33 @@
-car_color = "Blue"
-car_make = "Tesla"
+# car_color = "Blue"
+# car_make = "Tesla"
 
-if car_color == "Blue":
-     if car_make =="Tesla":
-        print("My car is a Blue Tesla")
-else:
-    print("My car is blue")
-elif: car_color == "Red":
-    print('My car is a ferrari')
-    else:
-    print("I just have a bike")
+# if car_color == "Blue":
+#      if car_make =="Tesla":
+#         print("My car is a Blue Tesla")
+# else:
+#     print("My car is blue")
+# elif: car_color == "Red":
+#         print('My car is a ferrari')
+# else:
+#     print("I just have a bike")
 
-    # for loop
-    for i in range(1,10):
+#     # for loop
+#     for i in range(1,10):
+#         print(i)
+
+# sports = ['soccer', 'basketball', 'hockey', 'football']
+# print(sports)
+
+# for item in sports:
+#     print(item)
+
+# i = 1
+# while : i < 6 
+#   print("I'm still going")
+#   1 += 1 
+
+for i in range(50):
+    if i > 24:
+        break
+    else: 
         print(i)
-
-sports = ['soccer', 'basketball', 'hockey', 'football']
-
-for item in sports:
-    print(item)
-
-i = 1
-white : i < 6 
-  print("I'm still going")
-  1 += 1 
