182d179f8af7f767ff34733bf924b41983825ca7
RachellSalcedo <scholar@paradigm.local>
2023-09-26 13:32:35 -0600

    if Statement

---
+++ if Statement.py
@@ -0,0 +1,8 @@
+# salary = int(input("What is your salary? "))
+# yos = int(input("How many years have you worked here? "))
+
+# if yos > 5:
+#     print(f"Your bonus amount will be {salary * 0.05}")
+# else:
+#     print("You don't qualify for the bonus yet.")
+
